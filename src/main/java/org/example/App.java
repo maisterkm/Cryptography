6590acd1d1010131dfd2bc5ff381c1145a876014
maister.alexander <maister.alexander@gmail.com>
2025-08-24 20:14:31 +0300

    implemented ceasar cipher

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,18 @@
 package org.example;
 
-public class App 
+import org.example.cipher.CeasarCipher;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        CeasarCipher cipher = new CeasarCipher();
+
+        String cipherText = cipher.encrypt(text, 3);
+        System.out.println(cipherText);
+
+        System.out.println(cipher.decrypt(cipherText, 3));
     }
 }
